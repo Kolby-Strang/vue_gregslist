bd84430a0905ea0c6ad328159554ed8fcc6a8cb4
Kolby-Strang <137920922+Kolby-Strang@users.noreply.github.com>
2023-10-25 22:34:17 -0600

    working on home machine

---
+++ src/pages/JobsPage.vue
@@ -0,0 +1,15 @@
+<template></template>
+
+
+<script>
+import { AppState } from '../AppState';
+import { computed, reactive, onMounted } from 'vue';
+export default {
+    setup() {
+        return {}
+    }
+};
+</script>
+
+
+<style lang="scss" scoped></style>
